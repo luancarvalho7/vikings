b7563ddbeb6c82097811d0a11735224ca885e81a
Luan Carvalho <luancarvprofissional@gmail.com>
2023-10-26 01:50:13 -0300

    jogem

--- src/assets/pages/chat/chat.jsx
+++ src/assets/pages/chat/chat.jsx
@@ -214,7 +214,7 @@ export function ChatPage({
             
             Tigrinho tá pagando muitoo <br/>
             ${sinal} <br/>
-            jogem até as ${timeLimit}
+            joguem até as ${timeLimit}
             `
         }
 
