a7bbc5e81422f1ca75dd04e91ca6d3a0ab0191ad
Luan Oak <99412966+luancarvalho7@users.noreply.github.com>
2023-12-11 17:10:33 -0300

    Update bannerSection.jsx

--- src/assets/pages/homepage/bannersSection/bannerSection.jsx
+++ src/assets/pages/homepage/bannersSection/bannerSection.jsx
@@ -30,7 +30,7 @@ export function BannerSection() {
                 <SwiperSlide><img src="https://i.postimg.cc/0jN4LT08/community.webp" alt="" style={{ cursor: "pointer" }}
                     onClick={() => { window.open("https://chat.whatsapp.com/FIqMuSVgTYg9j8nfcHd7Sa") }} /></SwiperSlide>
                 <SwiperSlide><img src="https://i.postimg.cc/brF58mm1/vip.webp" alt="" style={{ cursor: "pointer" }}
-                    onClick={() => { window.open("https://checkout.b4you.com.br/VY0X7rb_rE", "_blank")
+                    onClick={() => { window.open("https://vkngsapp.vercel.app/", "_blank")
                     }} /></SwiperSlide>
             </Swiper>
         </>
